7c49f46d0f500a4feecf1519108a95ae3d0206f3
Soham Das <sohamdas1587@gmail.com>
2019-08-29 02:32:30 +0530

    added query handling for second screen

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var cors = require('cors');
-// var bodyParser = require('body-parser');
-
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 var app = express();
-const port = 4000
+const port = 4000;
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./swagger/swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger/swagger.json");
 
-const routes = require('./routes')
+const routes = require("./routes");
 
 // app.use(bodyParser)
-app.use(express.json())
-app.use(cors())
-app.use('/dms', routes);
-app.use('/docs/v1', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/dms", routes);
+app.use("/docs/v1", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- api/routes/getCustomerByStatus/index.js
+++ api/routes/getCustomerByStatus/index.js
@@ -1,15 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const returnData = require('./index.json')
+const data = require("./index.json");
+let multer = require("multer")();
+router.post("/", multer.none(), function(req, res, next) {
+  let requestData = JSON.parse(req.body.data);
+  let response = {};
+  if (requestData.status === "Ready to Review") {
+    let customerInfo = data.customerInfo.filter(customer => {
+      if (
+        customer.bankReference === "Available" &&
+        customer.tradeReference === "Available"
+      ) {
+        return customer;
+      }
+    });
 
-router.post('/', function(req, res, next) {
-    // console.log(req.body)
-    let response = {
-        customerInfo: returnData.customerInfo,
-        requestStatus: "Ready to Review"
-    }
-    res.send(response)
-})
+    (response.customerInfo = customerInfo),
+      (response.requestStatus = "Ready to Review");
+  }
+  if (requestData.status === "Pending Information") {
+    let customerInfo = data.customerInfo.filter(customer => {
+      if (
+        customer.bankReference === "Pending" ||
+        customer.tradeReference === "Pending"
+      ) {
+        return customer;
+      }
+    });
 
-module.exports = router
+    (response.customerInfo = customerInfo),
+      (response.requestStatus = "Pending Information");
+  }
+  if (requestData.status === "Auto approved") {
+    let customerInfo = data.customerInfo.filter(customer => {
+      if (
+        customer.bankReference === "Available" &&
+        customer.tradeReference === "Available" &&
+        customer.approvedCreditLimit != undefined
+      ) {
+        return customer;
+      }
+    });
+
+    (response.customerInfo = customerInfo),
+      (response.requestStatus = "Auto approved");
+  }
+  res.send(response);
+});
+
+module.exports = router;
