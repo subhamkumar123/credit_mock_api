d2734806dfc90a24f4f16c2e1a4f33e11bb100a1
Soham Das <sohamdas1587@gmail.com>
2019-08-29 18:13:32 +0530

    added data and request processing

--- api/routes/getCustomerInsights/index.js
+++ api/routes/getCustomerInsights/index.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("multer")();
+const data = require("./index.json");
 
-const returnData = require('./index.json')
+router.post("/", multer.none(), function(req, res, next) {
+  let requestData = JSON.parse(req.body.data);
+  let returnData = data.find(customerData => {
+    if (
+      requestData.customerId == customerData.customerId &&
+      requestData.requestId == customerData.requestId
+    ) {
+      return customerData;
+    }
+  });
+  res.send(returnData);
+});
 
-router.post('/', function(req, res, next) {
-    res.send(returnData)
-})
-
-module.exports = router
-
+module.exports = router;
