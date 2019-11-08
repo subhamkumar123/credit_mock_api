d1e2d95c9d2dcdeae23596b287f7afe0c3171fcc
gyanesh.sharma <gyanesh.sharma@highradius.com>
2019-11-08 20:23:24 +0530

    Notes, Appove, Reject api added

---
+++ api/routes/rejectCustomer/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const data = require('./index.json');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  res.send(data);
+});
+
+module.exports = router;
