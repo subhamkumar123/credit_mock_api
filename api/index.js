e52f688f3cba80caadcc0e854464c2650a426daf
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-12-03 11:43:58 +0530

    Changed dms to crd and added data

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./swagger/swagger.json');
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger/swagger.json");
+const routes = require("./routes");
 
 const app = express();
 const port = 4000;
@@ -12,8 +12,8 @@ const port = 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/dms', routes);
-app.use('/docs/v1', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/crd", routes);
+app.use("/docs/v1", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
