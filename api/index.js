417e1a8d4a60c0395f4a72d869ecdbc38df83217
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-12-11 10:12:08 +0530

    :tada: Added websockets support

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDoc = require("./swagger/swagger.json");
-const routes = require("./routes");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const responseTime = require('response-time');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./swagger/swagger.json');
+const routes = require('./routes');
+const notificationWebSocket = require('./lib/notificationWebSocket');
 
 const app = express();
 const port = 4000;
 
-// app.use(bodyParser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/crd", routes);
-app.use("/docs/v1", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(responseTime());
+app.use(helmet());
+app.use('/crd', routes);
+app.use('/docs/v1', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`),);
+
+// Setup websocket connection
+notificationWebSocket.init(server);
