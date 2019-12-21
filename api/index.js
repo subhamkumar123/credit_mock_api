74cb78da576f025ca08077a0da9474d31c323578
kumar.subham <kumar.subham@highradius.com>
2019-12-22 00:55:47 +0530

    coolchangekiya

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const responseTime = require('response-time');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('./swagger/swagger.json');
-const routes = require('./routes');
-const notificationWebSocket = require('./lib/notificationWebSocket');
-const notificationData = require('../data/index');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const responseTime = require("response-time");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger/swagger.json");
+const routes = require("./routes");
+const notificationWebSocket = require("./lib/notificationWebSocket");
+const notificationData = require("../data/index");
 
 const app = express();
 const port = 4000;
@@ -17,15 +17,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(responseTime());
 app.use(helmet());
-app.use('/crd', routes);
-app.use('/docs/v1', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/crd", routes);
+
+app.use("/docs/v1", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // eslint-disable-next-line no-console
-const server = app.listen(port, () => console.log(`Mock server listening on port ${port}!`));
+const server = app.listen(
+    port,
+    () => console.log(`Mock server listening on port ${port}!`)
+
+    //kumarsubham cool
+);
 
 // Setup websocket connection
 const wsPromise = notificationWebSocket.init(server);
 
-wsPromise.then((socket) => {
-  notificationWebSocket.send(socket, notificationData);
+wsPromise.then(socket => {
+    notificationWebSocket.send(socket, notificationData);
 });
